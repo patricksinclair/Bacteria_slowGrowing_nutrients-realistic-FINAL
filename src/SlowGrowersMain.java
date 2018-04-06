a0a5329f2e92ef68a50759e459faf4b5caf0b12a
Patrick Sinclair <patricksinclair@hotmail.co.uk>
2018-04-06 14:41:02 +0100

    correct beta commit

--- src/SlowGrowersMain.java
+++ src/SlowGrowersMain.java
@@ -2,6 +2,7 @@ public class SlowGrowersMain {
 
     public static void main(String[] args){
         double specific_alpha = Math.log(11.5)/500.;
+
         BioSystem.exponentialGradient_spatialAndGRateDistributions();
 
     }
